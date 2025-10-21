3c314c0024dad959180ea0642871e51ed20ccdc4
ABHINANDAN GUPTA <137715892+abhinandan0245@users.noreply.github.com>
2025-10-21 20:12:57 +0530

    Modify CORS settings to allow all origins
    Updated CORS configuration to allow all origins.

--- server/src/main.js
+++ server/src/main.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 10000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: ["http://localhost:5173"] }));
+app.use(cors());
 
 connectDb();
 
