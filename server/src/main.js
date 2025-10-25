efd5299f756665353de644a36cb14f671aa864df
ABHINANDAN GUPTA <137715892+abhinandan0245@users.noreply.github.com>
2025-10-25 23:09:33 +0530

    Update main.js

--- server/src/main.js
+++ server/src/main.js
@@ -17,13 +17,16 @@ const port = process.env.PORT || 10000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: ["http://localhost:5173"] }));
-// app.use(cors());
 
-app.use(cors({
-  origin: ["https://taskmanagement100.netlify.app"], // aapki Netlify site
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // local Vite
+      "https://taskmanagement100.netlify.app", // live Netlify
+    ],
+    credentials: true,
+  })
+);
 
 
 
